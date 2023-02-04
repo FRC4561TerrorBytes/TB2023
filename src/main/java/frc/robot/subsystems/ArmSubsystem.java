28144b303fe8b5e558d320184bffd909bcde6de4
isaiahdrago <isaiah.menachem@gmail.com>
2023-02-04 15:50:33 -0500

    needs major work - isaiah

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -2,18 +2,26 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 
 public class ArmSubsystem extends SubsystemBase {
-  private WPI_TalonFX m_elbowMotor = new WPI_TalonFX(4);
-  private WPI_TalonFX m_shoulderMotor = new WPI_TalonFX(12);
+// private WPI_TalonFX m_elbowMotor = new WPI_TalonFX(4);
+// private WPI_TalonFX m_shoulderMotor = new WPI_TalonFX(12);
+  private CANSparkMax m_shoulderMotor = new CANSparkMax(0, MotorType.kBrushless);
+  private CANSparkMax m_elbowMotor = new CANSparkMax(11, MotorType.kBrushless);
+ private SparkMaxPIDController m_shoulderController;
+ private SparkMaxPIDController m_elbowController;
+
+
   private double m_targetShoulderPosition = 0.0;
   private double m_targetElbowPosition = 0.0;
 
@@ -44,11 +52,14 @@ public class ArmSubsystem extends SubsystemBase {
   }
 
   public ArmSubsystem() {
-    m_shoulderMotor.configFactoryDefault();
-    m_shoulderMotor.setNeutralMode(NeutralMode.Brake);
-    m_elbowMotor.configFactoryDefault();
-    m_elbowMotor.setNeutralMode(NeutralMode.Brake);
-    final TalonFXConfiguration elbowConfig = new TalonFXConfiguration();
+    m_shoulderController = m_shoulderMotor.getPIDController();
+    m_elbowController = m_elbowMotor.getPIDController();
+    m_shoulderMotor.restoreFactoryDefaults();
+    m_shoulderMotor.setIdleMode(IdleMode.kBrake);
+    m_elbowMotor.restoreFactoryDefaults();
+    m_elbowMotor.setIdleMode(IdleMode.kBrake);
+    m_elbowController.setReference(0, ControlType.kSmartMotion);
+    /*final NeoSteerConfiguration elbowConfig = new NeoSteerConfiguration<EncoderConfiguration>(11, null)
     elbowConfig.neutralDeadband = ArmConstants.ARM_MOTORS_NEUTRAL_DEADBAND;
     elbowConfig.primaryPID.selectedFeedbackSensor = TalonFXFeedbackDevice.IntegratedSensor.toFeedbackDevice();
     elbowConfig.slot0.kP = ArmConstants.ELBOW_PROPORTIONAL_GAIN;
@@ -75,18 +86,18 @@ public class ArmSubsystem extends SubsystemBase {
     shoulderConfig.forwardSoftLimitThreshold = 0;
     shoulderConfig.reverseSoftLimitEnable = true;
     shoulderConfig.reverseSoftLimitThreshold = ArmConstants.SHOULDER_ONLY_HORIZONTAL_TICKS * 2.0;
-    m_shoulderMotor.configAllSettings(shoulderConfig);
+    m_shoulderMotor.configAllSettings(shoulderConfig);*/
   }
 
   public void resetPosition() {
     System.out.println("reset");
-    m_shoulderMotor.setSelectedSensorPosition(0.0);
-    m_elbowMotor.setSelectedSensorPosition(0.0);
+    m_shoulderMotor.set(m_targetShoulderPosition);
+    m_elbowMotor.set(m_targetElbowPosition);
   }
 
   public void setArmSpeed(double shoulderSpeed, double elbowSpeed) {
-    m_shoulderMotor.set(ControlMode.PercentOutput, shoulderSpeed);
-    m_elbowMotor.set(ControlMode.PercentOutput, elbowSpeed);
+    m_shoulderMotor.set(shoulderSpeed);
+    m_elbowMotor.set(elbowSpeed);
   }
 
   /**
@@ -127,32 +138,32 @@ public class ArmSubsystem extends SubsystemBase {
    * {@link} {@link #setElbowPosition(double)}}.
    */
   void proceedToElbowPosition() {
-    double currentPos = m_elbowMotor.getSelectedSensorPosition();
+    /*double currentPos = m_elbowMotor.getSelectedSensorPosition();
     double degrees = (currentPos - ArmConstants.ELBOW_ONLY_HORIZONTAL_TICKS) / ArmConstants.ELBOW_TICKS_PER_DEGREE;
     double radians = java.lang.Math.toRadians(degrees);
     double cosineScalar = java.lang.Math.cos(radians);
     m_elbowMotor.set(
         ControlMode.MotionMagic, m_targetElbowPosition,
-        DemandType.ArbitraryFeedForward, ArmConstants.ELBOW_MAX_VOLTAGE_FF * cosineScalar);
+        DemandType.ArbitraryFeedForward, ArmConstants.ELBOW_MAX_VOLTAGE_FF * cosineScalar);*/
   }
 
   void proceedToShoulderPosition() {
-    double currentPos = m_shoulderMotor.getSelectedSensorPosition();
+    /*double currentPos = m_shoulderMotor.getSelectedSensorPosition();
     double degrees = (currentPos - ArmConstants.SHOULDER_ONLY_HORIZONTAL_TICKS) / ArmConstants.SHOULDER_TICKS_PER_DEGREE;
     double radians = java.lang.Math.toRadians(degrees);
     double cosineScalar = java.lang.Math.cos(radians);
     m_shoulderMotor.set(
         ControlMode.MotionMagic, m_targetShoulderPosition,
-        DemandType.ArbitraryFeedForward, ArmConstants.SHOULDER_MAX_VOLTAGE_FF * cosineScalar);
+        DemandType.ArbitraryFeedForward, ArmConstants.SHOULDER_MAX_VOLTAGE_FF * cosineScalar);*/
   }
 
   @Override
   public void periodic() {
-    SmartDashboard.putNumber("Elbow encoder", m_elbowMotor.getSelectedSensorPosition());
+    /*SmartDashboard.putNumber("Elbow encoder", m_elbowMotor.getSelectedSensorPosition());
     SmartDashboard.putNumber("Elbow voltage", m_elbowMotor.getMotorOutputVoltage());
     SmartDashboard.putNumber("Elbow current", m_elbowMotor.getStatorCurrent());
     SmartDashboard.putNumber("Shoulder encoder", m_shoulderMotor.getSelectedSensorPosition());
     SmartDashboard.putNumber("Shoulder voltage", m_shoulderMotor.getMotorOutputVoltage());
-    SmartDashboard.putNumber("Shoulder current", m_shoulderMotor.getStatorCurrent());
+    SmartDashboard.putNumber("Shoulder current", m_shoulderMotor.getStatorCurrent());*/
   }
 }
