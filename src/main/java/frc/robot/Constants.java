c42d81b72b2cffceb01e984a9dab40e6485b87b1
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2023-02-25 14:54:56 -0600

    Unfinished PathPlanning and Autos

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -191,4 +191,16 @@ public final class Constants {
   public static final double INTAKE_SPEED = 0.2;
   public static final double INTAKE_HOLD_SPEED = 0.05;
   public static final double INTAKE_SCORE_SPEEED = -0.1;
+
+  public static final double AUTO_X_KP = 1.0;
+  public static final double AUTO_X_KI = 0.0;
+  public static final double AUTO_X_KD = 0.0;
+
+  public static final double AUTO_Y_KP = 1.0;
+  public static final double AUTO_Y_KI = 0.0;
+  public static final double AUTO_Y_KD = 0.0;
+
+  public static final double AUTO_THETA_KP = 1.0;
+  public static final double AUTO_THETA_KI = 0.0;
+  public static final double AUTO_THETA_KD = 0.0;
 }
---
+++ src/main/java/frc/robot/commands/autonomous/ScoreCubeBalance.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.LEDSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
+import frc.robot.subsystems.ArmSubsystem.KnownArmPlacement;
+
+public class ScoreCubeBalance extends SequentialCommandGroup {
+  /** Creates a new ScoreCubeBalance. */
+  private DriveSubsystem m_driveSubsystem;
+  private ArmSubsystem m_armSubsystem;
+  private VisionSubsystem m_VisionSubsystem;
+  private IntakeSubsystem m_intakeSubsystem;
+  private LEDSubsystem m_ledSubsystem;
+  public ScoreCubeBalance() {
+    addCommands(
+      new AutoTrajectory(m_driveSubsystem, "ScoreCubeBalance1", 1, 1).getCommandAndStop(),
+      new RunCommand(() -> m_VisionSubsystem.centerAprilTag(0), m_VisionSubsystem),
+      new RunCommand(() -> m_armSubsystem.setKnownArmPlacement(KnownArmPlacement.SCORE_HIGH), m_armSubsystem),
+      new AutoTrajectory(m_driveSubsystem, "ScoreCubeBalance2", 1, 1).getCommandAndStop()
+        //FIXME: Add auto balance
+    );
+  }
+}
