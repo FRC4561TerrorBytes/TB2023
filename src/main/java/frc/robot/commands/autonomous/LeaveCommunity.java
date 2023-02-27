41f9c70763d531be620136444ce96af95323339e
Teesh <teesh.s93@gmail.com>
2023-02-27 18:56:20 -0500

    Stow Arm Before Auto

--- src/main/java/frc/robot/commands/autonomous/LeaveCommunity.java
+++ src/main/java/frc/robot/commands/autonomous/LeaveCommunity.java
@@ -6,14 +6,18 @@ import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ArmSubsystem.KnownArmPlacement;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ArmSubsystem.KnownArmPlacement;
 
 public class LeaveCommunity extends SequentialCommandGroup {
   /** Creates a new LeaveCommunity. */
   private DriveSubsystem m_driveSubsystem = new DriveSubsystem();
+  private ArmSubsystem m_armSubsystem = new ArmSubsystem();
 
   public LeaveCommunity() {
     addCommands(
+      new RunCommand(() -> m_armSubsystem.setKnownArmPlacement(KnownArmPlacement.STOWED), m_armSubsystem),
       new AutoTrajectory(m_driveSubsystem, "LeaveCommunity", 1, 1).getCommandAndStop()
     );
   }
