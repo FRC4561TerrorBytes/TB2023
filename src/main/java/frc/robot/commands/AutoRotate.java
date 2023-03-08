d4bb0d7e5ceb087e7fefb46607d8855dd5e558e1
Naren <naren@something.com>
2023-03-07 19:54:52 -0500

    Removed an unused import

--- src/main/java/frc/robot/commands/AutoRotate.java
+++ src/main/java/frc/robot/commands/AutoRotate.java
@@ -6,7 +6,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
