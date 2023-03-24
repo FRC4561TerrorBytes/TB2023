ece3f3b7671fa99d21549ffc29c52727f2236f31
Justin Merrill <jmerrill23@rthighschool.org>
2023-03-23 20:32:09 -0400

    Theta Controller shouldn't have continuous input

--- src/main/java/frc/robot/commands/autonomous/AutoTrajectory.java
+++ src/main/java/frc/robot/commands/autonomous/AutoTrajectory.java
@@ -4,15 +4,10 @@
 
 package frc.robot.commands.autonomous;
 
-import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
-import com.pathplanner.lib.PathPoint;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
-
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Constants;
@@ -24,6 +19,13 @@ public class AutoTrajectory {
   PathPlannerTrajectory m_pathPlannerTrajectory;
   PPSwerveControllerCommand m_swerveControllerCommand;
 
+  /**
+   * Creates a new PathPlanner trajectory for swerve modules to follow in autonomous
+   * @param driveSubsystem
+   * @param autoPathName
+   * @param maxSpeedMetersPerSec
+   * @param maxAccelerationMetersPerSecSquared
+   */
   public AutoTrajectory(DriveSubsystem driveSubsystem, String autoPathName, double maxSpeedMetersPerSec,
       double maxAccelerationMetersPerSecSquared) {
     this.m_driveSubsystem = driveSubsystem;
@@ -36,36 +38,8 @@ public class AutoTrajectory {
     PIDController yController = new PIDController(Constants.AUTO_Y_KP, Constants.AUTO_Y_KI, Constants.AUTO_Y_KD);
     PIDController thetaController = new PIDController(Constants.AUTO_THETA_KP, Constants.AUTO_THETA_KI,
         Constants.AUTO_THETA_KD);
-    thetaController.enableContinuousInput(-Math.PI, Math.PI);
-
-    m_swerveControllerCommand = new PPSwerveControllerCommand(
-        m_pathPlannerTrajectory,
-        m_driveSubsystem::getPose,
-        Constants.DRIVE_KINEMATICS,
-        xController,
-        yController,
-        thetaController,
-        m_driveSubsystem::setModuleStates,
-        m_driveSubsystem);
-  }
-
-  public AutoTrajectory(DriveSubsystem driveSubsystem, double maxSpeedMetersPerSec,
-      double maxAccelerationMetersPerSecSquared,
-      Translation2d targetTranslation, Rotation2d targetRotation) {
-    m_driveSubsystem = driveSubsystem;
-    m_pathPlannerTrajectory = PathPlanner.generatePath(
-        new PathConstraints(maxSpeedMetersPerSec, maxAccelerationMetersPerSecSquared),
-        new PathPoint(new Translation2d(), m_driveSubsystem.getRotation2d(), new Rotation2d()),
-        new PathPoint(targetTranslation, new Rotation2d(), targetRotation));
-
-    // Auto PID Controllers
-    PIDController xController = new PIDController(Constants.AUTO_X_KP, Constants.AUTO_X_KI, Constants.AUTO_X_KD);
-    xController.setTolerance(0.01);
-    PIDController yController = new PIDController(Constants.AUTO_Y_KP, Constants.AUTO_Y_KI, Constants.AUTO_Y_KD);
-    yController.setTolerance(0.01);
-    PIDController thetaController = new PIDController(Constants.AUTO_THETA_KP, Constants.AUTO_THETA_KI,
-        Constants.AUTO_THETA_KD);
-    thetaController.setTolerance(0.1);
+    //FIXME: TRY WITHOUT CONTINUOUS INPUT
+    //thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
     m_swerveControllerCommand = new PPSwerveControllerCommand(
         m_pathPlannerTrajectory,
