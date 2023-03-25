a4f44642ebecf1c5ef87437cd9700012b1cbeede
Steve <sfwasleski@att.net>
2023-03-25 17:55:19 -0400

    Simplified LED usage

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -2,20 +2,17 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.GameState;
-import frc.robot.GameState.CenteredState;
 import frc.robot.GameState.GamePiece;
 
 /**
  * The LED subsystem observes the {@link GameState} and sets the human player
- * and driver feedback LEDs to signal important game state changes. While not
- * holding a game piece, that is the next critical activity is at the loading
- * zone at the far end of the field, the half of the strip on the front of the
- * arm is used to signal the human players and the half of the strip on
- * the back of the arm is used to signal the drivers. When holding a game piece,
- * that is the next critical activity is in the community, the front LEDs are
- * used to signal the drivers.
+ * and driver feedback LEDs to signal important game state changes. The LEDs on
+ * the front of the arm signal the game piece being held or desired. The LEDs on
+ * the back of the arm are blue when a game piece is held and off, otherwise.
  * 
  * <p>
  * Note that the two strips on the robot arm are driven as one strip. Both
@@ -33,12 +30,8 @@ public class LEDSubsystem extends SubsystemBase {
   /** Cached {@link GameState} singleton. */
   private final GameState m_gameState;
 
-  /** Avoid unneeded and potentially costly game piece LED updates. */
-  private GamePiece m_lastGamePieceApplied = null;
-  /** Avoid unneeded and potentially costly centered state LED updates. */
-  private CenteredState m_lastCenteredStateApplied = null;
-  /** Avoid unneeded and potentially costly game piece held LED updates. */
-  private Boolean m_lastGamePieceHeldApplied = null;
+  /** Used to create onTrue and onFalse edges for enum change. */
+  private GamePiece m_lastGamePieceDesiredApplied = null;
 
   /**
    * Creates a new {@link LEDSubsystem}.
@@ -48,6 +41,59 @@ public class LEDSubsystem extends SubsystemBase {
     m_led.setLength(m_ledBuffer.getLength());
     m_led.setData(m_ledBuffer);
     m_led.start();
+    createGameStateBindings();
+    // Initialize LEDs to starting game state.
+    setFrontToGamePiece(m_gameState.getGamePieceDesired());
+    if (m_gameState.isGamePieceHeld()) {
+      setBackHalfLED(0, 0, 255);
+    }
+  }
+
+  /**
+   * Create and bind triggers for {@link GameState} change to LED update.
+   */
+  private void createGameStateBindings() {
+    // Trigger for game piece held changes.
+    final Trigger gamePieceHeldTrigger = new Trigger(m_gameState::isGamePieceHeld);
+    // On game piece held, show blue on back of arm.
+    gamePieceHeldTrigger.onTrue(new InstantCommand(() -> {
+      setBackHalfLED(0, 0, 255);
+    }));
+    // On game piece gone, turn off back of arm.
+    gamePieceHeldTrigger.onFalse(new InstantCommand(() -> {
+      setBackHalfLED(0, 0, 0);
+    }));
+
+    // Change the game piece indication of the front of the arm
+    // anytime it changes in game state.
+    final Trigger gamePieceDesiredTrigger = new Trigger(
+        () -> (m_gameState.getGamePieceDesired() != m_lastGamePieceDesiredApplied));
+    gamePieceDesiredTrigger
+        .onTrue(new InstantCommand(() -> {
+          final GamePiece gamePiece = m_gameState.getGamePieceDesired();
+          setFrontToGamePiece(gamePiece);
+          m_lastGamePieceDesiredApplied = gamePiece;
+        }));
+  }
+
+  /**
+   * Sets the LEDs on the front of the arm to match the parameter.
+   * 
+   * @param gamePiece the {@link GamePiece} being held or needed.
+   */
+  private void setFrontToGamePiece(final GamePiece gamePiece) {
+    switch (gamePiece) {
+      case CONE:
+        setFrontHalfLED(140, 40, 0);
+        break;
+      case CUBE:
+        setFrontHalfLED(62, 13, 115);
+        break;
+      default:
+        // Bug in GameState if we get here.
+        setFrontHalfLED(0, 0, 0);
+        break;
+    }
   }
 
   /**
@@ -77,99 +123,5 @@ public class LEDSubsystem extends SubsystemBase {
       m_ledBuffer.setRGB(i, r, g, b);
     }
     m_led.setData(m_ledBuffer);
-  }
-
-  /**
-   * This implementation monitors the {@link GameState} and sets the LEDs
-   * accordingly.
-   * 
-   * <p>
-   * {@inheritDoc}
-   */
-  @Override
-  public void periodic() {
-    // Holding a game piece or not drives the decisions of where to place feedback.
-    final boolean isGamePieceHeld = m_gameState.isGamePieceHeld();
-
-    // If the game piece held state has changed, make sure all feedback gets applied
-    // this cycle.
-    if ((m_lastGamePieceHeldApplied == null)
-        || (m_lastGamePieceHeldApplied.booleanValue() != isGamePieceHeld)) {
-      m_lastGamePieceApplied = null;
-      m_lastCenteredStateApplied = null;
-      m_lastGamePieceHeldApplied = null;
-    }
-
-    if (isGamePieceHeld) {
-      // Show game piece held since we have one and always on back.
-      // Only apply LED update if it has not already been done.
-      if (((m_lastGamePieceHeldApplied == null)
-          || (m_lastGamePieceHeldApplied.booleanValue() != isGamePieceHeld))) {
-        m_lastGamePieceHeldApplied = isGamePieceHeld;
-        setBackHalfLED(0, 0, 255);
-      }
-    } else {
-      // Nothing to apply for not held, but match last applied state.
-      m_lastGamePieceHeldApplied = Boolean.FALSE;
-
-      // Handle current game piece type for human player.
-      // Only shown if not holding a game piece and always on front.
-      final GamePiece gamePiece = m_gameState.getGamePieceDesired();
-      // Only apply LED update if it has not already been done.
-      if (m_lastGamePieceApplied != gamePiece) {
-        m_lastGamePieceApplied = gamePiece;
-        switch (gamePiece) {
-          case CONE:
-            setFrontHalfLED(140, 40, 0);
-            break;
-          case CUBE:
-            setFrontHalfLED(62, 13, 115);
-            break;
-          default:
-            // Bug in GameState if we get here.
-            setFrontHalfLED(0, 0, 0);
-            break;
-        }
-      }
-    }
-
-    // This state always shows up on LEDs but may be front or back.
-    // It will be the side not used above.
-    final CenteredState centeredState = m_gameState.getCenteredState();
-    // Only apply LED update if it has not already been done.
-    if (m_lastCenteredStateApplied != centeredState) {
-      m_lastCenteredStateApplied = centeredState;
-      switch (centeredState) {
-        case NOTCENTERED:
-          if (isGamePieceHeld) {
-            setFrontHalfLED(255, 0, 0);
-          } else {
-            setBackHalfLED(255, 0, 0);
-          }
-          break;
-        case PARTIAL:
-          if (isGamePieceHeld) {
-            setFrontHalfLED(251, 156, 0);
-          } else {
-            setBackHalfLED(251, 156, 0);
-          }
-          break;
-        case CENTERED:
-          if (isGamePieceHeld) {
-            setFrontHalfLED(0, 255, 0);
-          } else {
-            setBackHalfLED(0, 255, 0);
-          }
-          break;
-        default:
-          // NONE or a bug in GameState would get us here.
-          if (isGamePieceHeld) {
-            setFrontHalfLED(0, 255, 0);
-          } else {
-            setBackHalfLED(0, 0, 0);
-          }
-          break;
-      }
-    }
   }
 }
