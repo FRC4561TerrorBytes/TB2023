5af7b761dc72f9bc146f42c1073971a7764de7e2
Manbir Guron <mnguron19@gmail.com>
2023-03-31 00:30:55 -0400

    Revert "Initialized LED Subsystem"
    This reverts commit de7db637d10f8e487753ae4da38ab4d0bbff646e.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -353,7 +353,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    m_LEDSubsystem.init();
     // An example command will be run in autonomous
     return null;
   }
