84150de449a4311c6851068cb849899520a394c5
Steve <sfwasleski@att.net>
2023-04-01 10:26:17 -0400

    Accel limiting logic cleaned up some

--- src/main/java/frc/robot/commands/SquareUpAndDrive.java
+++ src/main/java/frc/robot/commands/SquareUpAndDrive.java
@@ -19,6 +19,8 @@ public class SquareUpAndDrive extends CommandBase {
   private static final TrapezoidProfile.Constraints MAX_VEL_ACCEL = new TrapezoidProfile.Constraints(2.0, 1.0);
   private static final double ALLOWED_ACCEL_PER_20MS_TIMESLICE = 0.02 * MAX_VEL_ACCEL.maxAcceleration;
   private static final double MINIMUM_FORWARD_VELOCITY = 0.1;
+  /** Set to false to turn off forward accel limiting. */
+  private static boolean LIMIT_FORWARD_ACCEL = true;
 
   final DriveSubsystem m_driveSubsystem;
   final PIDController m_rotationController = new PIDController(0.03, 0, 0);
@@ -61,34 +63,8 @@ public class SquareUpAndDrive extends CommandBase {
 
     // Get the forward but limit the acceleration.
     double forwardVelocity = m_forwardSupplier.getAsDouble();
-    if (Math.abs(m_lastForwardVelocity) >= MAX_VEL_ACCEL.maxVelocity) {
-      // Already at maximum
-      if (Math.abs(forwardVelocity) >= MAX_VEL_ACCEL.maxVelocity) {
-        // Request is maximum or more, stay at max.
-        forwardVelocity = m_lastForwardVelocity;
-      }
-    } else {
-      // Changing velocity but not at max yet.
-      if (m_lastForwardVelocity == 0.0) {
-        // Special case just starting.
-        if (forwardVelocity != 0.0) {
-          // The request is to start moving, first go to the minimum.
-          forwardVelocity = MINIMUM_FORWARD_VELOCITY * Math.signum(forwardVelocity);
-        }
-      } else {
-        // Was already moving but may want to change.
-        final double requestedDelta = forwardVelocity - m_lastForwardVelocity;
-        double limitedDelta = Math.min(ALLOWED_ACCEL_PER_20MS_TIMESLICE, Math.abs(requestedDelta))
-            * Math.signum(requestedDelta);
-        forwardVelocity = m_lastForwardVelocity + limitedDelta;
-        if (Math.abs(forwardVelocity) > MAX_VEL_ACCEL.maxVelocity) {
-          // Just calculated going over max velocity.
-          forwardVelocity = MAX_VEL_ACCEL.maxVelocity * Math.signum(forwardVelocity);
-        } else if (Math.abs(forwardVelocity) < MINIMUM_FORWARD_VELOCITY) {
-          // Just slowed to less than minimum, stop going forward.
-          forwardVelocity = 0.0;
-        }
-      }
+    if (LIMIT_FORWARD_ACCEL) {
+      forwardVelocity = limitAccel(forwardVelocity);
     }
 
     m_driveSubsystem.drive(
@@ -98,6 +74,29 @@ public class SquareUpAndDrive extends CommandBase {
         true);
 
     m_lastForwardVelocity = forwardVelocity;
+  }
+
+  private double limitAccel(double forwardVelocity) {
+    final double requestedDelta = forwardVelocity - m_lastForwardVelocity;
+    double limitedDelta = Math.min(ALLOWED_ACCEL_PER_20MS_TIMESLICE, Math.abs(requestedDelta))
+        * Math.signum(requestedDelta);
+    forwardVelocity = m_lastForwardVelocity + limitedDelta;
+    // Check edge conditions.
+    final double absForwardVelocity = Math.abs(forwardVelocity);
+    final double signumForwardVelocity = Math.signum(forwardVelocity);
+    if (absForwardVelocity > MAX_VEL_ACCEL.maxVelocity) {
+      // Just calculated going over max velocity.
+      forwardVelocity = MAX_VEL_ACCEL.maxVelocity * signumForwardVelocity;
+    } else if (absForwardVelocity < MINIMUM_FORWARD_VELOCITY) {
+      if (m_lastForwardVelocity == 0.0) {
+        // Just starting, use minimum velocity.
+        forwardVelocity = MINIMUM_FORWARD_VELOCITY * signumForwardVelocity;
+      } else {
+        // Just slowed to less than minimum, stop going forward.
+        forwardVelocity = 0.0;
+      }
+    }
+    return forwardVelocity;
   }
 
   // Called once the command ends or is interrupted.
