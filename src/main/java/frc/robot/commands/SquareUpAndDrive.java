1884238170477176d536454cac272d4c08d89cde
Steve <sfwasleski@att.net>
2023-04-03 19:17:35 -0400

    Switch to SlewRateLimiter - basic

--- src/main/java/frc/robot/commands/SquareUpAndDrive.java
+++ src/main/java/frc/robot/commands/SquareUpAndDrive.java
@@ -7,31 +7,20 @@ package frc.robot.commands;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class SquareUpAndDrive extends CommandBase {
-  /**
-   * Not actually using a trapezoid profile. Using the constraint types is,
-   * however, a convenient way to define our limits.
-   */
-  private static final TrapezoidProfile.Constraints MAX_VEL_ACCEL = new TrapezoidProfile.Constraints(2.0, 1.0);
-  private static final double ALLOWED_ACCEL_PER_20MS_TIMESLICE = 0.02 * MAX_VEL_ACCEL.maxAcceleration;
-  private static final double MINIMUM_FORWARD_VELOCITY = 0.1;
-  /** Set to false to turn off forward accel limiting. */
-  private static boolean LIMIT_FORWARD_ACCEL = true;
-
-  final DriveSubsystem m_driveSubsystem;
-  final PIDController m_rotationController = new PIDController(0.03, 0, 0);
-  final DoubleSupplier m_forwardSupplier;
-  double m_lastForwardVelocity = 0.0;
-  final DoubleSupplier m_strafeSupplier;
+  private final DriveSubsystem m_driveSubsystem;
+  private final PIDController m_rotationController = new PIDController(0.03, 0, 0);
+  private final DoubleSupplier m_forwardSupplier;
+  private final SlewRateLimiter m_forwardLimiter = new SlewRateLimiter(1.0);
+  private final DoubleSupplier m_strafeSupplier;
 
   public SquareUpAndDrive(final DriveSubsystem driveSubsystem,
       final DoubleSupplier forwardSupplier,
       final DoubleSupplier strafeSupplier) {
-    // Use addRequirements() here to declare subsystem dependencies.
     m_driveSubsystem = driveSubsystem;
     m_forwardSupplier = forwardSupplier;
     m_strafeSupplier = strafeSupplier;
@@ -47,7 +36,7 @@ public class SquareUpAndDrive extends CommandBase {
     final double absAngle = Math.abs(m_driveSubsystem.getPose().getRotation().getDegrees());
     final boolean closerTo0 = (180.0 - absAngle) > absAngle;
     m_rotationController.setSetpoint(closerTo0 ? 0.0 : 180.0);
-    m_lastForwardVelocity = 0.0;
+    m_forwardLimiter.reset(0.0);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -62,41 +51,13 @@ public class SquareUpAndDrive extends CommandBase {
     final double strafeVelocity = m_strafeSupplier.getAsDouble();
 
     // Get the forward but limit the acceleration.
-    double forwardVelocity = m_forwardSupplier.getAsDouble();
-    if (LIMIT_FORWARD_ACCEL) {
-      forwardVelocity = limitAccel(forwardVelocity);
-    }
+    final double forwardVelocity = m_forwardLimiter.calculate(m_forwardSupplier.getAsDouble());
 
     m_driveSubsystem.drive(
         forwardVelocity,
         strafeVelocity,
         rotationRate,
         true);
-
-    m_lastForwardVelocity = forwardVelocity;
-  }
-
-  private double limitAccel(double forwardVelocity) {
-    final double requestedDelta = forwardVelocity - m_lastForwardVelocity;
-    double limitedDelta = Math.min(ALLOWED_ACCEL_PER_20MS_TIMESLICE, Math.abs(requestedDelta))
-        * Math.signum(requestedDelta);
-    forwardVelocity = m_lastForwardVelocity + limitedDelta;
-    // Check edge conditions.
-    final double absForwardVelocity = Math.abs(forwardVelocity);
-    final double signumForwardVelocity = Math.signum(forwardVelocity);
-    if (absForwardVelocity > MAX_VEL_ACCEL.maxVelocity) {
-      // Just calculated going over max velocity.
-      forwardVelocity = MAX_VEL_ACCEL.maxVelocity * signumForwardVelocity;
-    } else if (absForwardVelocity < MINIMUM_FORWARD_VELOCITY) {
-      if (m_lastForwardVelocity == 0.0) {
-        // Just starting, use minimum velocity.
-        forwardVelocity = MINIMUM_FORWARD_VELOCITY * signumForwardVelocity;
-      } else {
-        // Just slowed to less than minimum, stop going forward.
-        forwardVelocity = 0.0;
-      }
-    }
-    return forwardVelocity;
   }
 
   // Called once the command ends or is interrupted.
