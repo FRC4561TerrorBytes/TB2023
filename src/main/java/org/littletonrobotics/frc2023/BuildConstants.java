3b7235427f8903df76961bf2221b9cc1962795d3
ethanprivette <112294818+ethanprivette@users.noreply.github.com>
2023-08-29 18:27:32 -0400

    fix cone mid position

--- src/main/java/org/littletonrobotics/frc2023/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2023/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TB2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 285;
-  public static final String GIT_SHA = "d58502f367789eae303f61f20a72104f5c200565";
-  public static final String GIT_DATE = "2023-08-26 16:02:29 EDT";
+  public static final int GIT_REVISION = 286;
+  public static final String GIT_SHA = "d9adfd80d8e836cbd2f280cb88a9ab65e44a8207";
+  public static final String GIT_DATE = "2023-08-29 17:41:31 EDT";
   public static final String GIT_BRANCH = "AdvantageKit-TEst";
-  public static final String BUILD_DATE = "2023-08-29 17:36:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1693345006674L;
+  public static final String BUILD_DATE = "2023-08-29 18:12:47 EDT";
+  public static final long BUILD_UNIX_TIME = 1693347167586L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
